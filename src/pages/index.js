145d9837feadd9c943e1e1246db3ee12f585e041
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-01 14:36:18 +0430

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,11 @@
 import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import  from './'
+import  from './'
+import  from './'
+import  from './'
+import  from './'
 
 export {
     Home
