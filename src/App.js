f19ee9de6adc853d382b67b7e5a498f4d244b842
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-01 11:14:24 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import styled from 'styled-components'
+
 function App() {
   return <h4>comfy sloth starter</h4>
 }
