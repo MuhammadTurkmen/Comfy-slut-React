fc80afcb4d4d5b231dd868bb70abc2b3a0f4a0bc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 14:26:57 +0430

    init

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -5,6 +5,8 @@ const ErrorPage = () => {
   return <Wrapper className='page-100'>
     <section>
       <h1>404</h1>
+      <h3>Sorry, the page you tried connot be found</h3>
+      <Link to="/" className="btn">back home</Link>
     </section>
   </Wrapper>
 }
