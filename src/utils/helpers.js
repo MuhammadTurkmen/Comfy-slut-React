61f6f5a1c8ea92378626f3faae8c5b6d899ce095
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 10:58:09 +0430

    init

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,8 @@
 export const formatPrice = (number) => {
-    const newNumber = Intl.NumberFormat('en-US', {
+   return Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD'
     }).format(number / 100)
-    return newNumber 
 } 
 
 export const getUniqueValues = () => {}
