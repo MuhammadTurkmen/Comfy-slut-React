2b487b8301f1436445cf1f46f6b0053a3c3ab7bf
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 09:14:49 +0430

    init

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,8 @@
 import React from 'react'
 const Error = () => {
-  return <div className='section secion-center text-center'></div>
+  return <div className='section secion-center text-center'>
+    <h2>there was an error...</h2>
+  </div>
 }
 
 export default Error
