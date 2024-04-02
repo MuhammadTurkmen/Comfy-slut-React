ce50ecdfa19d455311d70f604c13bd843cefa1df
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 10:46:51 +0430

    init

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,6 +24,9 @@ const Sidebar = () => {
             <Link to={url}>{text}</Link>
           </li>
         })}
+        <li>
+            <Link to="/checkout">checkout</Link>
+          </li>
       </ul>
     </aside>
   </SidebarContainer>
