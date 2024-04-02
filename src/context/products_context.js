b25d5523b56179867ddb6e097738a407d68685b9
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 07:41:55 +0430

    init

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -36,8 +36,11 @@ export const ProductsProvider = ({ children }) => {
 
   const fetchProducts = async(url) => {
     dispatch({type: GET_PRODUCTS_BEGIN})
-    const response = await axios.get(url)
-    console.log(response);
+    try {
+      const response = await axios.get(url)
+    } catch (error) {
+      
+    }
   }
 
   useEffect(() => {
