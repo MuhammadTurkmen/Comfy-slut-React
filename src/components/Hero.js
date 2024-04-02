fa1d7e60d15b7365006d58ce95be8bf22eadb997
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 13:05:16 +0430

    init

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
   return <Wrapper className='section-center'>
-
+    <article className='content'></article>
   </Wrapper>
 }
 
