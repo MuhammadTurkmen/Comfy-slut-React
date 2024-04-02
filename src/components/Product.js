2ec35d36699b88012e91c2f9296c37c95f34fbd8
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 09:41:54 +0430

    init

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,6 +8,7 @@ const Product = ({image, name, price, id}) => {
   return <Wrapper>
     <div className='container'>
       <img src={image} alt={name}/>
+      <Link to={`/products/${id}`} className="link"></Link>
     </div>
   </Wrapper>
 }
