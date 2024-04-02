d84e75a1dc1d24979e312753246997c8cb5e7524
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 14:05:57 +0430

    init

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  
+  const {isSidebarOpen, closeSidebar} = useProductsContext()
 
   const isOpen = false
   return <SidebarContainer>
