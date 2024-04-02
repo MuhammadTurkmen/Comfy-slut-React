6be92b2551787c8ef9f36bbf65f6ed6702c57d3f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 14:24:04 +0430

    init

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 const ErrorPage = () => {
-  return <h4>error page</h4>
+  return <Wrapper className='page-100'>error page</Wrapper>
 }
 
 const Wrapper = styled.main`
