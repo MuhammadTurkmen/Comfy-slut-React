09d7ca8c1ba879d7fc4b5cd11ef885596302cbcb
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 13:07:49 +0430

    init

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,14 @@ const Hero = () => {
         design your <br />
         comfort zone
       </h1>
-      <p></p>
+      <p>is simply dummy text of the printing and
+         typesetting industry. Lorem Ipsum has been the industry's 
+         standard dummy text ever since the 1500s, when an unknown printer 
+         took a galley of type and scrambled it to make a type specimen book. 
+         It has survived not only five centuries, but also the leap into electronic 
+         typesetting, remaining essentially unchanged. It was popularised 
+        in the 1960s with the release of Letraset sheets
+      </p>
     </article>
   </Wrapper>
 }
