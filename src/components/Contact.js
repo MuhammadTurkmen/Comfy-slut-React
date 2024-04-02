fe7e196acffdd55fb8a205a95033cb4b32e50bdd
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 09:32:30 +0430

    init

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,7 @@ const Contact = () => {
         </p>
         <form className='contact-form'>
           <input type='email' className='form-input' placeholder='enter email'/>
+          <button type='submit' className='submit-btn'>subscribe</button>
         </form>
       </div>
     </div>
