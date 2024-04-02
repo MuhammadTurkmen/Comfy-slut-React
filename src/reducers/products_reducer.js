4541b62468dc4a075d698667e68a324d3716f5dc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 08:45:17 +0430

    init

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -23,8 +23,11 @@ const products_reducer = (state, action) => {
     const featured_products = action.payload.filter((product) => product.featured === true)
     return {...state, products_loading: false, products: action.payload, featured_products}
   }
+
+  if(action.type === GET_PRODUCTS_ERROR) {
+    return {...state, products_loading: false, products_error: true}
+  }
  
-  return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
