7d0bdf7925ffd2df2d9b459ed90100bc4a68ab35
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 08:42:00 +0430

    init

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -21,6 +21,7 @@ const products_reducer = (state, action) => {
   }
   if(action.type === GET_PRODUCTS_SUCCESS) {
     const featured_products = action.payload.filter((product) => product.featured === true)
+    return {...state, products_loading: false, products: action.payload, featured_products}
   }
  
   return state
