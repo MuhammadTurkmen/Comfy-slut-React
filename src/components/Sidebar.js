79b8a9719ce37aceb142f40957c6996553141cf0
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-02 13:27:35 +0430

    init

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,6 +9,8 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
+  const data = useProductsContext()
+  console.log(data);
   const isOpen = false
   return <SidebarContainer>
     <aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
