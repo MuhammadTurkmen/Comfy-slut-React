90ba018d5b6042211cd8c1169ac6d0b98c6d856e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-03 18:00:12 +0430

    init

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>contact section</Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
