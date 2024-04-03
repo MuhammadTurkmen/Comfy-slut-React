105fa4ecd820b5edb28bca104988014232e8b8a3
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-03 17:29:54 +0430

    init

--- src/components/Services.js
+++ src/components/Services.js
@@ -18,11 +18,13 @@ const Services = () => {
         <div className='services-center'>
           {services.map((service) => {
             const {id, icon, title, text} = service
-            return <article key={id} className='service'>
-              <sapn className='icon'>{icon}</sapn>
-              <h4>{title}</h4>
-              <p>{text}</p>
-            </article>
+            return (
+              <article key={id} className='service'>
+                <sapn className='icon'>{icon}</sapn>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
           })}
         </div>
       </div>
