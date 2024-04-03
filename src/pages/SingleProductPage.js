158dece9ffe945b350cbe62816d2a5599c02a8f2
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-03 17:15:55 +0430

    init

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -44,7 +44,7 @@ const SingleProductPage = () => {
   }
   const {name, price, description, stock, stars, reviews, id: sku, company, images} = product
   return <Wrapper>
-    
+    <PageHero title={name}/>
   </Wrapper>
 }
 
