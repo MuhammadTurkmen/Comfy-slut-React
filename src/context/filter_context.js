ade4a3504ad2e0265058ecc98368ca754ea9240b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 18:07:20 +0430

    init

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -20,6 +20,7 @@ const initialState = {
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <FilterContext.Provider value='filter context'>
       {children}
