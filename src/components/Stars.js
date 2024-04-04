0b8c69b9fc7578650a0a229e2cbb3265f1a1418d
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 16:32:02 +0430

    init

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -6,7 +6,7 @@ const Stars = ({stars, reviews}) => {
   const tempStars = Array.from({length: 5}, (_, index) => {
     const number = index + 0.5
     return (
-      <span>{stars >= index + 1 ? <BsStarFill /> : stars >= number ? <BsStarHalf /> : <BsStar />}</span>
+      <span key={index}>{stars >= index + 1 ? <BsStarFill /> : stars >= number ? <BsStarHalf /> : <BsStar />}</span>
     )
   })
   
