880a66ee56e04b435413174e0429014fa06294b4
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 18:09:09 +0430

    init

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <ProductsProvider>
-        <App />
+        <FilterProvider>
+            <App />
+        </FilterProvider>
     </ProductsProvider>
 
 );
