0c903e0a30604ed2df26c3fdabab0ad2fc6b9f08
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 20:58:49 +0430

    init

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,6 +5,11 @@ import ListView from './ListView'
 
 const ProductList = () => {
   const {filtered_products: products, grid_view} = useFilterContext()
+  if(products.length < 1) {
+    <h5 style={{textTransform: 'none'}}>
+      Sorry, no product matched your search
+    </h5>
+  }
   return <GridView products={products}>
     
   </GridView>
