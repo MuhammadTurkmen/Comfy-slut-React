2fba9e1d0b36da5bac8dc2e5258fcc30f53f5c34
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 16:21:17 +0430

    init

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,14 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 const Stars = ({stars, reviews}) => {
+  // <span>{stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}</span>
+
+  
   return <Wrapper>
     <div className='stars'>
       {/* star */}
-      <span>{stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}</span>
-      <span>{stars >= 2 ? <BsStarFill /> : stars >= 1.5 ? <BsStarHalf /> : <BsStar />}</span>
-      <span>{stars >= 3 ? <BsStarFill /> : stars >= 2.5 ? <BsStarHalf /> : <BsStar />}</span>
-      <span>{stars >= 4 ? <BsStarFill /> : stars >= 3.5 ? <BsStarHalf /> : <BsStar />}</span>
-      <span>{stars === 5 ? <BsStarFill /> : stars >= 4.5 ? <BsStarHalf /> : <BsStar />}</span>
+      
       {/* end of star */}
     </div>
     <p className='reviews'>({reviews} customer reviews)</p>
