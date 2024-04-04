952661b931829cef1ce54207d1e924654ea14dcf
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 17:07:31 +0430

    init

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
-const AmountButtons = () => {
+const AmountButtons = ({increase, decrease, amount}) => {
   return <h4>amount buttons </h4>
 }
 
