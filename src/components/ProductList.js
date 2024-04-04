b0d2a240f14adc08bb5a05d24f16443b42c0acea
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-04 20:56:53 +0430

    init

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,7 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const {filtered_products: products} = useFilterContext()
+  const {filtered_products: products, grid_view} = useFilterContext()
   return <GridView products={products}>
     
   </GridView>
