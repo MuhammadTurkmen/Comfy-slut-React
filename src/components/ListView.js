6ccd059edc1a750b512640fe035d394460cdaee0
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 21:07:00 +0430

    init

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -11,8 +11,10 @@ const ListView = ({products}) => {
         <div>
           <h4>{name}</h4>
           <h5 className='price'>{formatPrice(price)}</h5>
+          <p>{description}</p>
+          <Link to={} ></Link>
         </div>
-      </article> 
+      </article>  
     })}
   </Wrapper>
 }
