528f0e619b5b590f8152c79a1df8c4932f2446eb
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 07:04:35 +0430

    init

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -21,9 +21,9 @@ const Sort = () => {
       <label htmlFor='sort'>sort by</label>
       <select name='sort' id='sort' className='sort-input'>
         <option value='price-lowest'>price (lowest)</option>
-        <option value='price-lowest'>price (lowest)</option>
-        <option value='price-lowest'>price (lowest)</option>
-        <option value='price-lowest'>price (lowest)</option>
+        <option value='price-highest'>price (highest)</option>
+        <option value='name-a'>name (a-z)</option>
+        <option value='name-z'>name (z-a)</option>
       </select>
     </form>
   </Wrapper>
