3bbff2bae5af84fddbd9115210842d4af3053ef1
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 07:34:24 +0430

    init

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -40,6 +40,7 @@ export const FilterProvider = ({ children }) => {
     // for demostration
     // const name = e.target.name
     const value = e.target.value
+    dispatch({type: UPDATE_SORT, payload: value})
   }
   
   
