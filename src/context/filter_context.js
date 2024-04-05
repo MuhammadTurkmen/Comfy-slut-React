afe7931383cf0a909895ee652b2bd5d4f66515d5
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 09:07:18 +0430

    init

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,7 +16,16 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
-  sort: 'price-lowest'
+  sort: 'price-lowest',
+  filters: {
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0
+  }
 }
 
 const FilterContext = React.createContext()
