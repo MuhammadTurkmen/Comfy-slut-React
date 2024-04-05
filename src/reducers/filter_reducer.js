2acefc752d7c38bee610e8acc2aff07a7dc255dc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 07:41:27 +0430

    init

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -24,6 +24,10 @@ const filter_reducer = (state, action) => {
   if(action.type === UPDATE_SORT) {
     return {...state, sort: action.payload}
   }
+
+  if(action.type === SORT_PRODUCTS) {
+    return {...state}
+  }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
