156c2db50518c137db6e5eee2450ed1a6343b362
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 07:29:54 +0430

    init

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -26,7 +26,7 @@ const Sort = () => {
     <hr />
     <form>
       <label htmlFor='sort'>sort by</label>
-      <select name='sort' id='sort' className='sort-input'>
+      <select name='sort' id='sort' className='sort-input' value={sort} onChange={updateSort}>
         <option value='price-lowest'>price (lowest)</option>
         <option value='price-highest'>price (highest)</option>
         <option value='name-a'>name (a-z)</option>
