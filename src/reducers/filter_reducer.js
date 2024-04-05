947e27a72424ce13586e028fd6ad3202c719ad38
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 11:32:00 +0430

    init

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -58,6 +58,7 @@ const filter_reducer = (state, action) => {
 
   if(action.type === UPDATE_FILTERS) {
     const {name, value} = action.payload
+    return {...state, filters: {...state.filters, [name]: value}}
   }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
