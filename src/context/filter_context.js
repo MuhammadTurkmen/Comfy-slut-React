1d634c6480a6b6f429d2ce4edcfd411e2d270ee2
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 11:26:36 +0430

    init

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -60,7 +60,7 @@ export const FilterProvider = ({ children }) => {
   const updateFilters = (e) => {
     let name = e.target.name
     let value = e.target.value
-    dispatch({type: UPDATE_FILTERS})
+    dispatch({type: UPDATE_FILTERS, payload:{name, value}})
   }
 
   const clearFilters = () => {
