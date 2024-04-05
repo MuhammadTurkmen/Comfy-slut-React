8f2b62a53686d2a3fed836aec774f848053b272c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-05 09:12:17 +0430

    init

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,7 +11,13 @@ import {
 
 const filter_reducer = (state, action) => {
   if(action.type === LOAD_PRODUCTS) {
-    return {...state, all_products: [...action.payload], filtered_products: [...action.payload]}
+    let maxPrice = action.payload.map((p) => p.price)
+    maxPrice = Math.max(...maxPrice)
+    return {
+      ...state, 
+      all_products: [...action.payload], 
+      filtered_products: [...action.payload]
+    }
   }
 
   if(action.type === SET_GRIDVIEW) {
