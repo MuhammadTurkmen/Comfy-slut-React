960dd25472d5c803eed67a55c6a0108e8b300a9b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-06 17:48:12 +0430

    init

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -70,6 +70,9 @@ export const FilterProvider = ({ children }) => {
     if(name === 'price') {
       value = Number(value)
     }
+    if(name === 'shipping') {
+      value = e.target.checked
+    }
     dispatch({type: UPDATE_FILTERS, payload:{name, value}})
   }
 
