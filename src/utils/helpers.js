91462eee4583c162c6af3d101f8f0c5739944e14
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-06 12:21:16 +0430

    init

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,5 +7,5 @@ export const formatPrice = (number) => {
 
 export const getUniqueValues = (data, type) => {
     let unique = data.map((item) => item[type])
-    console.log(unique);
+    return ['all', ...new Set(unique)]
 }
