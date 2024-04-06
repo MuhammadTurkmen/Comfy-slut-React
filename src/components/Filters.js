9118d0bfde9f3101ecab068c4f19658d5f27cea7
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-06 17:56:59 +0430

    init

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -128,7 +128,7 @@ const Filters = () => {
         </div>
         {/* end of shipping */}
       </form>
-      <button type='button' className='clear-btn'></button>
+      <button type='button' className='clear-btn' onClick={clearFilters}>clear filters</button>
     </div> 
   </Wrapper>
 }
