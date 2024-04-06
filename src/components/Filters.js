448d9087632a050ffffa97f6385f5db2efd9796b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-06 14:13:53 +0430

    init

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -59,6 +59,7 @@ const Filters = () => {
         </div>
         {/* end of categories */}
         {/* companies */}
+        <div className='form-control'></div>
         {/* end of companies */}
       </form>
     </div> 
