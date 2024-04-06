32e11a369046fcadbb9fc1f58b6faad7e880f5bf
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-06 12:48:04 +0430

    init

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -23,7 +23,7 @@ const Filters = () => {
 
   const categories = getUniqueValues(all_products, 'category')
   const companies = getUniqueValues(all_products, 'company')
-  const colors = getUniqueValues(all_products, 'color')
+  const colors = getUniqueValues(all_products, 'colors')
   console.log(colors);
   
   
