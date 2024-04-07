327fb5207fcbb343499288f767786cbbf54d29db
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-07 07:56:54 +0430

    init

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -37,12 +37,13 @@ export const CartProvider = ({ children }) => {
 
   // remove item
   const removeItem = (id) => {
+    dispatch({type: REMOVE_CART_ITEM, payload: id})
   }
   // toggle amount
   const toggleAmount = (id, value) => {}
   // clear cart
   const clearCart = () => {
-
+    dispatch({type: CLEAR_CART})
   } 
 
   useEffect(() => {
