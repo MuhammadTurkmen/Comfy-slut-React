eda7d744eddf0dfbcabce70afb3240fcf9153896
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-07 20:37:25 +0430

    init

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -21,7 +21,7 @@ export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
   
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
+    <CartContext.Provider value={{...state}}>{children}</CartContext.Provider>
   )
 }
 // make sure use
