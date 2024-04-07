81185ec5f716fe864197a00b33603d1fe515751e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-07 21:27:58 +0430

    init

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom'
 import { CartContent, PageHero } from '../components'
 
 const CartPage = () => {
+  const {cart} = useCartContext
+  if(cart.length < 1) {
+    return <Wrapper className='page-100'>
+      <div className='empty'>
+        <h2>Your cart is empty </h2>
+      </div>
+    </Wrapper>
+  }
   return <h4>cart page</h4>
 }
 
