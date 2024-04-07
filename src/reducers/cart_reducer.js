347742f5917d94d49a17b8fef05f3d9ddf52891e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-07 20:55:19 +0430

    init

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,7 +7,10 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
-  return state
+  if(action.type === ADD_TO_CART) {
+    const {id, color, amount, product} = action.payload
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
