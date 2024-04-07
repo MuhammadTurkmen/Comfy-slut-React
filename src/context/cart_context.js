fa6861ab3d3d91b79bc270098b0bdf4e476ba070
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-07 21:38:14 +0430

    init

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,18 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart')
+  if(cart) {
+    return JSON.parse(localStorage.getItem('cart'))
+  }
+  else {
+    return []
+  }
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_item: 0,
   total_amount: 0,
   shipping_fee: 534,
