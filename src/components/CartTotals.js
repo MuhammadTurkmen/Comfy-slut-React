25104486203c0c1c4ecf3690c4376128b506ac82
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-07 21:56:17 +0430

    init

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -12,7 +12,10 @@ const CartTotals = () => {
       <article>
         <h5>subtotal : <span>{formatPrice(total_amount)}</span></h5>
         <p>shipping fee : <span>{formatPrice(shipping_fee)}</span></p>
+        <hr />
+        <h4>order total : <span>{formatPrice(total_amount + shipping_fee)}</span></h4>
       </article>
+      <Link to="checkout" className="btn">proceed to checkout</Link>
     </div>
   </Wrapper>
 }
