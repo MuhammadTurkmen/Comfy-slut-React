9ec938a3a90d4eac7664910ab7bdadbda1e86ac9
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 14:55:37 +0430

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 
-import { Home, SingleProduct, Cart, Checkout, Error,About, Products, PrivateRoute } from './pages'
+import { Home, SingleProduct, Cart, Checkout, Error,About, Products, PrivateRoute, AuthWrapper } from './pages'
 
 function App() {
   return <Router>
