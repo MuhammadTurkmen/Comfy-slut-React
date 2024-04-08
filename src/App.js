a8ee03060d154f2585c48b19912cc6dd3acaa50b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 14:56:14 +0430

    init

--- src/App.js
+++ src/App.js
@@ -6,34 +6,37 @@ import { Navbar, Sidebar, Footer } from './components'
 import { Home, SingleProduct, Cart, Checkout, Error,About, Products, PrivateRoute, AuthWrapper } from './pages'
 
 function App() {
-  return <Router>
-    <Navbar />
-    <Sidebar />
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/cart">
-        <Cart />
-      </Route>
-      <Route exact path="/products">
-        <Products />
-      </Route> 
+  return (
 
-      <Route exact path="/products/:id" children={<SingleProduct />} />
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route> 
 
-      <PrivateRoute exact path="/checkout">
-        <Checkout />
-      </PrivateRoute> 
-      <Route path="*">
-        <Error />
-      </Route>
-    </Switch>
-    <Footer />
-  </Router>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+
+        <PrivateRoute exact path="/checkout">
+          <Checkout />
+        </PrivateRoute> 
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
