b69dcbcee8ef28ac7a875bbe92e50fb77903cb85
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 16:29:18 +0430

    init

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
   const {closeSidebar} = useProductsContext()
-  const {total_items} = useCartContext()
+  const {total_items, clearCart} = useCartContext()
   const {loginWithRedirect, myUser, logout} = useUserContext()
   return <Wrapper className='cart-btn-wrapper'>
     <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
@@ -23,7 +23,11 @@ const CartButtons = () => {
     {myUser ?  <button 
       type='button' 
       className='auth-btn' 
-      onClick={() => logout({returnTo: window.location.origin})}
+      onClick={() => {
+        clearCart()
+        logout({returnTo: window.location.origin})
+      
+      }}
       >
       Logout <FaUserMinus />
     </button> : 
