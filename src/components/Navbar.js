82efb4b0b70cd9de42a125950ff328da429a511c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 11:28:19 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
   const {openSidebar} = useProductsContext()
+  const {myUser} = useUserContext()
   return <NavContainer>
     <div className='nav-center'>
       <div className='nav-header'>
