86a0f75de4066dd3756c3f583044ec84d3ed9a48
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 14:42:04 +0430

    init

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -8,14 +8,9 @@ export const UserProvider = ({ children }) => {
   const [myUser, setMyUser] = useState(null)
   
   useEffect(() => {
-    if(isAuthenticated) {
-      setMyUser(user)
-    }
-    else {
-      setMyUser(false)
-    }
+    setMyUser(user)
 
-  }, [isAuthenticated])
+  }, [user])
   
   return (
     <UserContext.Provider value={{loginWithRedirect, logout, myUser}}>{children}</UserContext.Provider>
