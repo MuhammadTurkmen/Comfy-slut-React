86255be79f0177931b57b801de9dc5590fa74fd3
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 12:29:43 +0430

    init

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -7,7 +7,7 @@ import { useUserContext } from '../context/user_context';
 const PrivateRoute = ({children, ...rest}) => {
   const {myUser} = useUserContext()
   return <Route {...rest} render={() => {
-    
+    return myUser ? children : <Redirect to='/' />
   }}></Route>;
 };
 export default PrivateRoute;
