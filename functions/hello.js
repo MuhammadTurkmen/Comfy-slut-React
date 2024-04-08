c6ce9c3e736b3fa6790fe24e39c19e9594694d6c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 16:11:40 +0430

    init

--- functions/hello.js
+++ functions/hello.js
@@ -8,6 +8,6 @@ const items = [
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: 'hello world'
     }
 }
