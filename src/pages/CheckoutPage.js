b5409e0e17d363c47336e07b2e96455ee328541b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 16:21:42 +0430

    init

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -6,9 +6,11 @@ import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
+  const {cart} = useCartContext()
   return <main>
     <PageHero title='checkout'/>
     <Wrapper className='page'>
+      {cart.length < 1 ? <div></div>: <StripeCheckout />}
       <h1>checkout here</h1>
     </Wrapper>
   </main>
