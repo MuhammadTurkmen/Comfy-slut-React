63494f8b80f417067200ffb23b76c4e4369f7dcb
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 17:59:52 +0430

    init

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -20,6 +20,24 @@ const CheckoutForm = () => {
 }
 
 const StripeCheckout = () => {
+   const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#32325d',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  };
+  
   return (
     <Wrapper>
       <Elements stripe={promise}>
