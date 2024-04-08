14fec012e97369f4eba106c5d6d0d84da11cc735
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 12:23:20 +0430

    init

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,6 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({children, ...rest}) => {
+  const {myUser} = useUserContext()
   return <h4>Private Route</h4>;
 };
 export default PrivateRoute;
