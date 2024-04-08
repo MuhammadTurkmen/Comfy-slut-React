68e39e01613178d2337deda48c1c50ae9c6bd487
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-08 09:18:47 +0430

    init

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
+// dev-rka3n06c3krrricb.us.auth0.com
+// XBff7bXEFEgH66loWu1y7kJecqy6w4xp
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
