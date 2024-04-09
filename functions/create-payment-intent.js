8ffd7e8787b5c41c7b3bcac061cf8f0bea7da6af
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-09 20:00:52 +0430

    init

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -3,7 +3,7 @@
 
 
 exports.handler = async function(event, context) {
-    console.log(event);
+    const {cart, shipping_fee, total_amount} = JSON.parse(event.body)
     return {
         statusCode: 200,
         body: 'payment intent'
