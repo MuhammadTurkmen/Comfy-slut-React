33900e8ef4d293de6a41e80bd4b12c7b6bc6cd5b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-09 21:51:39 +0430

    init

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -81,6 +81,7 @@ const CheckoutForm = () => {
       : <article>
         <h4>Hello, {myUser && myUser.name}</h4>
         <p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+        <p>Test Card Number : 4242 4242 4242 4242</p>
       </article>
     }
     <form id='payment-form' onSubmit={handleSubmit}>
