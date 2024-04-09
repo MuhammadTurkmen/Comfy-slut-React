cc755f8f297e385eae34e88fc74ddc51afafbbc1
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-09 09:20:30 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 
@@ -11,29 +11,9 @@ function App() {
       <Router>
         <Navbar /> 
         <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-          <Route exact path="/products">
-            <Products />
-          </Route> 
-
-          <Route exact path="/products/:id" children={<SingleProduct />} />
-
-          <PrivateRoute exact path="/checkout">
-            <Checkout />
-          </PrivateRoute> 
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          
+        </Routes>
         <Footer />
       </Router>
     </AuthWrapper>
