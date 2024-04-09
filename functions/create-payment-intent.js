ff51e5d5a6bbe6da5f2a876273bd3f80f92f8ac1
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-09 21:30:13 +0430

    init

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -18,12 +18,16 @@ exports.handler = async function(event, context) {
                 amount: calculateOrderAmount(),
                 currency: 'usd'
             })
+
             return {
                 statusCode: 200,
                 body: JSON.stringify({clientSecret: paymentIntent.client_secret})
             }
         } catch (error) {
-            
+            return {
+                statusCode: 500,
+                body: JSON.stringify({error: error.message})
+            }
         }
     }
     return {
